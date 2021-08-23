f26829fcc7424fd6092cd6d9cc7974f19bfb721d
Tamir <1tamir198@gmail.com>
2021-08-23 18:20:58 +0300

    Finished files

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -1,35 +1,37 @@
 const canvas = document.getElementById('draw')
 const canvasContext = canvas.getContext('2d');
-let isLineWidthGrowing = true;
-let hslHue  = 0;
+
+let isCanvasLineWidthGrowing = true;
+let hslHue = 0;
 
 var lastMousePos = { x: 0, y: 0 };
 
-window.onload = function() {
-  resizeCanvas();
-};
-
+document.addEventListener('resize',resizeCanvas);
 document.addEventListener('mousemove', drawWithMouseOnCanvas);
 document.addEventListener('mousedown', setNewMousePos);
 document.addEventListener('mouseenter', setNewMousePos);
 
-function setNewMousePos(e) {
-  lastMousePos.x = e.clientX;
-  lastMousePos.y = e.clientY;
+window.onload = ()=>{
+canvasContext.lineWidth = 100;
+resizeCanvas();
+}
+
+function setNewMousePos(mouseEvent) {
+  lastMousePos.x = mouseEvent.clientX;
+  lastMousePos.y = mouseEvent.clientY;
 }
 
 function resizeCanvas() {
-  console.log("sd");
   canvasContext.canvas.width = window.innerWidth;
   canvasContext.canvas.height = window.innerHeight;
 }
 
 function drawWithMouseOnCanvas(e) {
-  //Dont draw if mouse was no pressed 
+  //Dont draw if mouse was not pressed 
   if (!e.buttons) return;
 
   canvasContext.beginPath();
-  canvasContext.lineWidth = getLineSize(canvasContext.lineWidth);
+  canvasContext.lineWidth = getLineWidth(canvasContext.lineWidth);
   canvasContext.lineCap = 'round';
   canvasContext.lineJoin = 'round';
   canvasContext.strokeStyle = getStrokeColor();
@@ -44,15 +46,15 @@ function getStrokeColor() {
   if (hslHue >= 360) {
     hslHue = 0;
   }
-  return `hsl(${hslHue+= 1/10}, 100%, 50%)`;
+  return `hsl(${hslHue += 1}, 100%, 50%)`;
 }
 
-function getLineSize(size) {
-  if (size >= 100) {
-    isLineWidthGrowing = false;
+function getLineWidth(size) {
+  if (size > 100) {
+    isCanvasLineWidthGrowing = false;
   }
-  if (size <= 1) {
-    isLineWidthGrowing = true;
+  if (size == 1) {
+    isCanvasLineWidthGrowing = true;
   }
-  return (isLineWidthGrowing) ? (size + 1 / 10) : (size - 1 / 10);
+  return (isCanvasLineWidthGrowing) ? (size + 1) : (size - 1);
 }
