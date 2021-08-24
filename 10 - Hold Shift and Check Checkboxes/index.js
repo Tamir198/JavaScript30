1032928b3026c6a64ab0b3dffa4d37c7579130e2
Tamir <1tamir198@gmail.com>
2021-08-24 12:21:12 +0300

    Finished files

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,6 +1,6 @@
 let isFirstClickOnCheckbox = true;
 let isShiftPressed = false;
-let firstCheckboxIndex;
+let firstCheckBoxIndex = 0;
 const checkBoxes = [...document.querySelectorAll('.inbox input')];
 
 checkBoxes.forEach(checkbox => checkbox.addEventListener("click", clickCheckBoxesInrange));
@@ -8,16 +8,19 @@ checkBoxes.forEach(checkbox => checkbox.addEventListener("click", clickCheckBoxe
 function updateFirstCheckboxClicked(e) {
   for (let i = 0, len = checkBoxes.length; i < len; i++) {
     if (isFirstClickOnCheckbox && checkBoxes[i] == e.target) {
-      firstCheckboxIndex = i;
+      return i;
     };
   }
 }
 
 function clickCheckBoxesInrange(e) {
-  updateFirstCheckboxClicked(e);
+  if (isFirstClickOnCheckbox) {
+    firstCheckBoxIndex = updateFirstCheckboxClicked(e);
+  }
+
   isFirstClickOnCheckbox = false;
   if (e.shiftKey) {
-    for (let i = firstCheckboxIndex; i < checkBoxes.length; i++) {
+    for (let i = firstCheckBoxIndex; i < checkBoxes.length; i++) {
       if (e.target == checkBoxes[i]) {
         break;
       }
